968b057683944a2b6a7e20a4af0989dde2ca8286
Miraslau.Rabikau <mira2408@mail.ru>
2021-09-30 19:10:07 +0300

    create LSP principle

--- 3.ts
+++ 3.ts
@@ -1,31 +1,39 @@
 //В данном коде происходит нарушение Liskov Substitution принципа
 //Исправьте следующий код так, чтобы данный принцип соблюдался, сохранив исходную функциональность кода.
 
-class Rectangle {
+abstract class Figure {
+  abstract calculateArea(): number
+}
+
+class Rectangle extends Figure{
   public width: number;
   public height: number;
 
   constructor(width: number, height: number) {
+    super();
     this.width = width;
     this.height = height;
   }
-}
 
-class Circle {
-  public radius: number;
-
-  constructor(radius: number) {
-    this.radius = radius;
+  calculateArea(): number {
+    return this.width + this.height
   }
 }
 
-function computeSpace(shapes: Array<Rectangle | Circle>) {
-  return shapes.reduce((calculatedArea, shape) => {
-    if (shape instanceof Rectangle) {
-      return calculatedArea + shape.width * shape.height;
-    }
-    if (shape instanceof Circle) {
-      return calculatedArea + shape.radius * Math.PI;
-    }
-  }, 0);
+class Circle extends Figure{
+  public radius: number;
+
+  constructor(radius: number) {
+    super();
+    this.radius = radius;
+  }
+  calculateArea(): number {
+    return Math.PI * Math.pow(this.radius, 2);
+  }
 }
+function computeSpace(shape: Figure) {
+  return shape.calculateArea()
+}
+
+console.log(computeSpace(new Circle(5)))
+console.log(computeSpace(new Rectangle(10, 10)))
